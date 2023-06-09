49ec21f07812bb9250aa028a7417ac591ea466d3
Renan Cruz <renanpc61@gmail.com>
2023-06-09 12:47:16 -0300

    atribuicao criada

---
+++ Atribuicao.cpp
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
+ */
+
+/* 
+ * File:   Atribuicao.cpp
+ * Author: Renan
+ * 
+ * Created on 9 de Junho de 2023, 11:46
+ */
+
+#include "Atribuicao.h"
+
+Atribuicao::Atribuicao() {
+}
+
+Atribuicao::Atribuicao(const Atribuicao& orig) {
+}
+
+Atribuicao::~Atribuicao() {
+}
---
+++ Atribuicao.h
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ */
+
+/* 
+ * File:   Atribuicao.h
+ * Author: Renan
+ *
+ * Created on 9 de Junho de 2023, 11:46
+ */
+
+#ifndef ATRIBUICAO_H
+#define ATRIBUICAO_H
+
+class Atribuicao {
+public:
+    Atribuicao();
+    Atribuicao(const Atribuicao& orig);
+    virtual ~Atribuicao();
+    
+    void Tempo();
+private:
+
+};
+
+#endif /* ATRIBUICAO_H */
+
